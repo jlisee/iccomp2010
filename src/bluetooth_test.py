31e0a9566ead9d2832c3c38e697c4c971e9e3d61
jlisee <none@none>
2010-08-19 23:29:13 -0400

    Updated test to allow sending a test frame to the device

--- src/bluetooth_test.py
+++ src/bluetooth_test.py
@@ -9,8 +9,9 @@ import serial
 # Project Imports
 import proto.messages_robocup_ssl_detection_pb2 as ssl_detect
 import proto.messages_robocup_ssl_wrapper_pb2 as ssl_wrapper
+import messages
 
-def openPort(devfile):
+def open_port(devfile):
     port = serial.Serial()
     port.setPort(devfile)
     port.setBaudrate(9600)
@@ -21,22 +22,37 @@ def openPort(devfile):
     port.open()
     return port
 
+def basic_test(port):
+    print 'Read:',port.read(5)
+    msg = 'Hello, Robot!'
+    port.write(msg)
+    print '"%s" sent' % msg
+
+def full_test(port):
+    field_info = messages.make_test_detectionframe()
+    print 'Sending full packet:\n',field_info
+    field_info.send_data(port)
+    print 'sent'
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
     # Parse arguments
     parser = optparse.OptionParser()
-    parser.set_defaults(devfile="/dev/rfcomm0")
+    parser.set_defaults(devfile="/dev/rfcomm0", fulltest = False)
     parser.add_option("-d", "--dev", dest="devfile",
                       help="Device file the NXT is connected to")
+    parser.add_option("-f", "--fulltest",  action="store_true", dest="fulltest",
+                      help="Send a test packet to python")
     (options, args) = parser.parse_args()
+    
+    port = open_port(options.devfile)
 
-    port = openPort(options.devfile)
-
-    print port.read(5)
-
-    port.write('Hello, Robot!')
+    if options.fulltest:
+        full_test(port)
+    else:
+        basic_test(port)
 
     port.close()
 
